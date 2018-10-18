27bbacc462165d74912768c2326ac1a5ddbcd9cc
Lucas Maciel - DTI <lucas.maciel@dtidigital.com.br>
2018-10-18 18:12:00 -0300

    Rota Home com informações do Gateway

--- src/main/java/gateway/Application.java
+++ src/main/java/gateway/Application.java
@@ -44,6 +44,25 @@ public class Application {
             .build();
     }
     
+    @RequestMapping("/")
+    public Mono<String> home() {
+        return Mono.just("<body>\r\n" + 
+        		"    <h1>Spring Cloud Gateway</h1>\r\n" + 
+        		"	<h2>Exemplo de uso do Spring Gateway para o Trabalho do curso de Arquitetura de Backend - Puc Minas 10/2018</h2>\r\n" + 
+        		"	<h4>Integrantes:</h4>\r\n" + 
+        		"	<ul>\r\n" + 
+        		"        <li>Lucas Maciel</li>\r\n" + 
+        		"        <li>Pedro Eustáquio</li>\r\n" + 
+        		"        <li>Raphael Fernandes</li>\r\n" + 
+        		"        <li>Wiliam Walder</li>\r\n" + 
+        		"	</ul>\r\n" + 
+        		"    <h4>Objetivo:</h4>\r\n" + 
+        		"    <div>O objetivo deste gateway é prover o roteamento das APIs de Livraria, autenticação, auditoria e pagamento (mock)</div>\r\n" + 
+        		"	<h4>Onde está o código:</h4>\r\n" + 
+        		"    <div>O código para este gateway pode ser encontrado no branch <strong>master</strong> do repositório do Github: https://github.com/1ucas/spring-api-gateway</div>\r\n" + 
+        		"</body>");
+    }
+    
     @RequestMapping("/fallback")
     public Mono<String> fallback() {
         return Mono.just("fallback");
